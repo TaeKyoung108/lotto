a5ca5a831d58572a68fba89587f50d4a73b35b31
TaeKyoung108 <rlaxorud0108@naver.com>
2023-05-08 20:35:41 +0900

    시작을 위한 commit

---
+++ src/test/java/com/lotto/LotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
